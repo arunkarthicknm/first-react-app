5e52121a1e7e5ea7b9844fc521e4148038e0ac2c
arunkarthicknm <arunkarthicknm@gmail.com>
2021-05-24 13:32:10 +0530

    Added main container

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -2,21 +2,15 @@ import React, { Component } from 'react';
 
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
-import DishDetail from './Dishdetailcomponent';
+
 
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          selectedDish: null
-      }
-  }
-
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
     
-  }
+    }
+
 
 
 
@@ -24,8 +18,8 @@ class Menu extends Component {
     const menu =this.props.dishes.map((dish) => {
         return (
           <div key={ dish.id } className="col-12 col-md-5 m-1">
-            <Card 
-              onClick={() => this.onDishSelect(dish)}>
+            <Card key={dish.id}
+              onClick={() => this.props.onClick(dish.id)}>
               <CardImg width="100%" src={dish.image} alt={dish.name} />
               <CardImgOverlay>
                   <CardTitle>{dish.name}</CardTitle>
@@ -34,18 +28,21 @@ class Menu extends Component {
           </div>
         );
     });
+  
 
     return (
         <div className="container">
             <div className="row">
                 {menu}
             </div>
-            <DishDetail dish={this.state.selectedDish} />
+            
         </div>
         
     );
   }
 }
+
+
           
   
   
