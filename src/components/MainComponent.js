4a0063510e592b297f9adf4769a62dbe35ab1134
arunkarthicknm <arunkarthicknm@gmail.com>
2021-05-24 13:40:30 +0530

    Added Container

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
+import Menu from './MenuComponents';
+import DishDetail from './Dishdetailcomponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
